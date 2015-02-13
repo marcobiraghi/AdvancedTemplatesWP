5d79559870c43ef3f6b3ecd9973af6889dfbfdc2
marcobiraghi <marco.biraghi@live.it>
2015-02-13 23:28:23 +0100

    Added FrameContainer, ViewPage and basic navigation helper

---
+++ WindowApplication/WindowApplication/Controls/FrameContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace WindowApplication.Controls
+{
+    public class FrameContainer : ContentControl
+    {
+        private ContentControl FirstContentPresenter;
+        private Frame AppFrame;
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            FirstContentPresenter = GetTemplateChild("FirstContentPresenter") as ContentControl;
+            if (FirstContentPresenter != null)
+                AppFrame = FirstContentPresenter.Content as Frame;
+        }
+    }
+}
